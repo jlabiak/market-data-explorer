6d4f28e4d7e959d9f3ac2825542ce7a74ba3efe8
John Labiak <johnlabiak@johns-mbp.mynetworksettings.com>
2022-08-10 18:17:44 -0400

    migrating to postgres db

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+import os
+import psycopg2
+
 START_DATE = '2000-01-01'
 END_DATE = '2022-07-01'
 PATH_TO_INDEX_COMPONENTS = '../data/index_components.csv'
-DB_URL = r'sqlite:///../data/stockdata.sqlite'
+DB_URL = os.environ['DATABASE_URL'] # set to 'postgresql://johnlabiak:@localhost/johnlabiak'
