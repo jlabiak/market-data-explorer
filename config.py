e2a3b7ff31b01e4dc6f653c69ba614afe1251021
John Labiak <johnlabiak@johns-mbp.mynetworksettings.com>
2022-08-10 18:30:22 -0400

    updating path to tickers

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import psycopg2
 
 START_DATE = '2000-01-01'
 END_DATE = '2022-07-01'
-PATH_TO_INDEX_COMPONENTS = '../data/index_components.csv'
+PATH_TO_INDEX_COMPONENTS = 'data/index_components.csv'
 DB_URL = os.environ['DATABASE_URL'] # set to 'postgresql://johnlabiak:@localhost/johnlabiak'
