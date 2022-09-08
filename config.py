5dcc0c6c1e3e7fa05d7ac08ab7192c5dd180a42a
John Labiak <johnlabiak@johns-mbp.mynetworksettings.com>
2022-09-08 14:06:59 -0400

    cleaning up config

--- config.py
+++ config.py
@@ -1,20 +1,5 @@
 import os
-import psycopg2
-import subprocess
 
-# Set to 'local' or 'heroku'
-RUN_ENV = 'local'
-
-START_DATE = '2010-01-01'
+START_DATE = '2011-01-01'
 PATH_TO_INDEX_COMPONENTS = 'data/index_components.csv'
-#DB_URL = 'sqlite:///./data/equity_data.db'
-#DB_URL = 'postgresql://localhost/johnlabiak'
-
-if RUN_ENV == 'local':
-	DB_URL = os.environ['DATABASE_URL']
-elif RUN_ENV == 'heroku':
-	HEROKU_APP_NAME = 'market-data-explorer'
-	DB_URL = subprocess.run(
-	    ['heroku', 'config:get', 'DATABASE_URL', '--app', HEROKU_APP_NAME],
-	    capture_output=True
-	).stdout 
+DB_URL = os.getenv('DATABASE_URL', 'postgresql://localhost/johnlabiak')
