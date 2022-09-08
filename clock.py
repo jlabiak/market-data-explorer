acd418777ab0c74155280eb47828d81968c410fa
John Labiak <johnlabiak@johns-mbp.mynetworksettings.com>
2022-09-08 17:48:03 -0400

    running job daily

--- clock.py
+++ clock.py
@@ -4,18 +4,17 @@ from data import get_prices_from_db
 from app import r
 import pickle
 import zlib
-import os
 import sys
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('cron', day_of_week='*', hour=3)
 def scheduled_job():
     print('Running scheduled job...')
     df = get_prices_from_db()
-    df = df[df.index > '2011-01-01']
+    df = df[df.index > config.START_DATE]
     compressed_df = zlib.compress(pickle.dumps(df))
-    print('Setting df of size {}'.format(sys.getsizeof(compressed_df) / 1e6))
+    print('Caching df of size {}'.format(sys.getsizeof(compressed_df) / 1e6))
     r.set('prices', compressed_df)
 
 sched.start()
