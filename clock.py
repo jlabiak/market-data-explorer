c8c53f0865a7ffa57c310e898b3da0889777ae4f
John Labiak <johnlabiak@johns-mbp.mynetworksettings.com>
2022-09-08 11:04:22 -0400

    checking size of compressed df

--- clock.py
+++ clock.py
@@ -1,21 +1,20 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import redis
 from data import get_prices_from_db
+from app import r
 import pickle
 import zlib
 import os
+import sys
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=3)
 def scheduled_job():
     print('Running scheduled job...')
     df = get_prices_from_db()
-    print('Connecting to Redis server at {}'.format(os.environ.get('REDIS_URL', '')))
-    r = redis.from_url(os.environ.get('REDIS_URL', ''))
-    print(r.ping())
-    #r = redis.from_url('redis://localhost:6379')
-    #r = redis.Redis(host='localhost', port=6379, db=0)
-    r.set('prices', zlib.compress(pickle.dumps(df)))
+    compressed_df = zlib.compress(pickle.dumps(df))
+    print('Setting df of size {}'.format(sys.getsizeof(compressed_df) / 1e6))
+    r.set('prices', compressed_df)
 
 sched.start()
